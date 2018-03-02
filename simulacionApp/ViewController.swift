80c8384a97544f8509e5f8afe1bed9d43c5a9412
Tania Ramon <tania-ramon-prats@hotmail.com>
2018-03-02 18:48:09 +0100

    Initial Commit

---
+++ simulacionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simulacionApp
+//
+//  Created by Tania Ramon on 2/3/18.
+//  Copyright © 2018 Tania Ramon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
